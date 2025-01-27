2158d8379fc9f5ac626ed2460cf6cbf039aeb445
zumrenur <zumrenursafak1@gmail.com>
2025-01-27 15:58:19 +0300

    eklenti

--- HairDesign/script.js
+++ HairDesign/script.js
@@ -59,4 +59,26 @@ window.addEventListener("scroll", function () {
       });
     });
 
+const col = document.querySelector('.col');
+
+// Görünürlük kontrolü
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Kaydırma olayı
+window.addEventListener('scroll', () => {
+  if (isInViewport(col)) {
+    col.classList.add('active');
+  } else {
+    col.classList.remove('active');
+  }
+});
+
  
